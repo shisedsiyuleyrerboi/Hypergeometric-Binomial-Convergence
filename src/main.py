67584b7b5efac66a7192f0e2d9b1d1b72e50f6c1
Francisco Luna Ratto <franciscolunaratto@icloud.com>
2025-09-26 15:37:09 -0500

    Update main.py

--- src/main.py
+++ src/main.py
@@ -6,42 +6,33 @@ import os
 os.system('cls')
 
 correct_selection = False
-while (correct_selection != True): 
+while (correct_selection != True):
     N_min = int(input('Minimum # of Simulations(N) = '))
     N_max = int(input('Maximum # of Simulations(N) = '))
     n = 10
-    k = int(input('# of Successes(k)           = '))
-    print(f'Sample Space(n)             = {n}')
+    k = 10
 
-    if (k >= 0 and N_min > 0 and N_max >= N_min):
+    if (N_min > 0 and N_max > N_min):
         correct_selection = True
 
 # Case 1 (p = 0.3, r = pN)
 def hypergeometric_function_case_1(N, n, k):
     p = 0.3
-    r = int(round(p * N))
-    if (k > r or (n-k) > (N-r) or n > N or k < 0 or k > n):
-        return 0
+    r = int(p * N)
     return (math.comb(r, k) * math.comb(N - r, n - k)) / math.comb(N, n)
 
 def binomial_function_case_1(n, k):
     p = 0.3
-    if (k < 0 or k > n):
-        return 0
     return math.comb(n, k) * (p**k) * (1 - p) ** (n - k)
 
 # Case 2 (r = 20, p = r/N)
 def hypergeometric_function_case_2(N, n, k):
     r = 20
-    if (k > r or (n-k) > (N-r) or n > N or k < 0 or k > n):
-        return 0
     return (math.comb(r, k) * math.comb(N - r, n - k)) / math.comb(N, n)
 
 def binomial_function_case_2(n, k, N):
     r = 20
     p = r / N
-    if (k < 0 or k > n):
-        return 0
     return math.comb(n, k) * (p**k) * (1 - p) ** (n - k)
 
 # VISUALIZATION #
